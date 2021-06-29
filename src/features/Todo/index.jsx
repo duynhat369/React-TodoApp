ea8e652e43d25d773f67d0b3fc821f9f599487c1
Duy Nhat <duynhat719@gmail.com>
2021-06-29 19:47:12 +0700

    Handle toggle state, remove item

--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -9,30 +9,60 @@ Todo.propTypes = {};
 
 function Todo(props) {
     const [todos, setTodos] = useState([])
+    const [inputUpdate, setInputUpdate] = useState('')
+
+    //toggle class updating
+    const todoNode = document.querySelector(".todo-app")
 
     //fetch api by axios
     useEffect(() => {
         //this side effect is executed once after rendering
         const fetchTodos = async () => {
             const todoData = await todoApi.getAll()
-
             setTodos(todoData)
         }
 
         fetchTodos()
     }, [])
 
-    //post input form value to api
-    const handleFormSubmit = (formValues) => {
+    //post input form value (form add)
+    const handleFormSubmit = async (formValues) => {
+        await todoApi.add(formValues)
+        const todoData = await todoApi.getAll()
+        setTodos(todoData)
+    }
 
-        const postTodos = async () => {
-            await todoApi.add(formValues)
-            const todoData = await todoApi.getAll()
+    //patch input form value (form update)
+    const handleFormUpdateSubmit = async (newFormValues) => {
+        todoNode.classList.toggle("updating")
 
-            setTodos(todoData)
-        }
+        // await todoApi.add(newFormValues)
+        // const todoData = await todoApi.getAll()
+        // setTodos(todoData)
+    }
 
-        postTodos()
+    //Edit item todo
+    const handleEditClick = (todo) => {
+        todoNode.classList.toggle("updating")
+        // const newValue = todo.title
+        setInputUpdate(todo.title)
+    }
+
+    //patch item todo
+    const handleUpdateStatus = async (todo) => {
+        //toggle state
+        todo.status = todo.status === "done" ? "new" : "done"
+
+        await todoApi.update(todo)
+        const todoData = await todoApi.getAll()
+        setTodos(todoData)
+    }
+
+    //delete todo form value
+    const handleDeleteClick = async (todo) => {
+        await todoApi.remove(todo.id)
+        const todoData = await todoApi.getAll()
+        setTodos(todoData)
     }
 
     //rendering
@@ -41,11 +71,20 @@ function Todo(props) {
             <div className="todo-heading">
                 <h2 className="todo-heading__title">Plans for today</h2>
                 <span className="todo-heading__counter">
-                    <TodoCounter />
+                    <TodoCounter todosLength={todos.length} />
                 </span>
             </div>
-            <TodoForm onSubmit={handleFormSubmit} />
-            <TodoList todos={todos} />
+            <TodoForm
+                inputUpdate={inputUpdate}
+                onSubmit={handleFormSubmit}
+                onUpdateSubmit={handleFormUpdateSubmit}
+            />
+            <TodoList
+                todos={todos}
+                onEditClick={handleEditClick}
+                onDeleteClick={handleDeleteClick}
+                onUpdateStatus={handleUpdateStatus}
+            />
         </div>
     );
 }
