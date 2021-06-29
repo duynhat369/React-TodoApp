3d4a4cf251c45be6a6e38eb5d1ae56b5944d7f50
Duy Nhat <duynhat719@gmail.com>
2021-06-29 19:49:30 +0700

    Functions handle: - toggle state (completed item and new item) - remove item

--- src/features/Todo/components/TodoCounter/index.jsx
+++ src/features/Todo/components/TodoCounter/index.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 TodoCounter.propTypes = {
-  
+    todosLength: PropTypes.number,
 };
 
+TodoCounter.defaultProps = {
+    todosLength: 0,
+}
+
 function TodoCounter(props) {
-  return (
-    <div>
-      
-    </div>
-  );
+    const { todosLength } = props
+    return (
+        <React.Fragment>
+            {todosLength}
+        </React.Fragment>
+    );
 }
 
 export default TodoCounter;
--- src/features/Todo/components/TodoForm/index.jsx
+++ src/features/Todo/components/TodoForm/index.jsx
@@ -3,21 +3,30 @@ import React, { useState } from 'react';
 import "./styles.scss";
 
 TodoForm.propTypes = {
+    inputUpdate: PropTypes.string.isRequired,
     onSubmit: PropTypes.func,
+    onUpdateSubmit: PropTypes.func,
 };
 
 TodoForm.defaultProps = {
     onSubmit: null,
+    onUpdateSubmit: null,
 }
-
 function TodoForm(props) {
-    const { onSubmit } = props
+    const { inputUpdate, onSubmit, onUpdateSubmit } = props
+
     const [input, setInput] = useState('')
+    const [newInputUpdate, setNewInputUpdate] = useState("")
 
     const handleInputChange = (e) => {
         setInput(e.target.value)
     }
 
+    const handleInputUpdateChange = (e) => {
+        setNewInputUpdate(e.target.value)
+    }
+
+    //handle submit form add todo
     const handleFormSubmit = (e) => {
         if (!onSubmit) return
 
@@ -25,7 +34,6 @@ function TodoForm(props) {
         e.preventDefault()
 
         const formValues = {
-            //id: Math.floor(Math.random() * 10000),
             title: input,
             status: "new",
         }
@@ -41,10 +49,28 @@ function TodoForm(props) {
         onSubmit(formValues)
     }
 
+    //handle submit form update todo
+    const handleFormUpdateSubmit = (e) => {
+        if (!onSubmit) return
+
+        //prevent loading browser
+        e.preventDefault()
+
+        const newFormValues = {
+            title: inputUpdate,
+        }
+        console.log("inputUpdate after: ", inputUpdate)
+        //validate input form value
+        if (!newFormValues.title || /^\s*$/.test(newFormValues.title)) {
+            return
+        }
+        console.log("title", newFormValues.title)
+
+        onUpdateSubmit(newFormValues)
+    }
+
     return (
         <React.Fragment>
-            {/* {counts} */}
-
             {/* form add */}
             <form className="todo-form-add" onSubmit={handleFormSubmit}>
                 <input
@@ -59,13 +85,15 @@ function TodoForm(props) {
             </form>
 
             {/* form update */}
-            <form className="todo-form-update">
+            <form className="todo-form-update" onSubmit={handleFormUpdateSubmit}>
                 <input
                     type="text"
-                    name="todo-input"
-                    className="todo-input"
-                //value=""
-                //onChange=""
+                    name="todo-input-update"
+                    className="todo-input-update"
+
+                    // nếu value là prop thì không thể onChange, nếu là state thì không set value vào input
+                    value={inputUpdate}
+                    onChange={handleInputUpdateChange}
                 />
                 <button className="todo-button"> UPDATE </button>
             </form>
