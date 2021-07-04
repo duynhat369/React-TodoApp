3f35031292ae77a40c08b533a9c9f556bde98493
Duy Nhat <duynhat719@gmail.com>
2021-07-04 19:28:39 +0700

    Remove handle functions: - write handle functions in the "rendering" instead write handle function

--- src/features/Todo/components/TodoList/index.jsx
+++ src/features/Todo/components/TodoList/index.jsx
@@ -1,42 +1,27 @@
-import classNames from 'classnames';
-import React from 'react';
-import { AiOutlineDelete, AiOutlineEdit, AiOutlineRollback } from 'react-icons/ai';
-import { MdDone } from 'react-icons/md';
+import classNames from "classnames";
+import React from "react";
+import {
+    AiOutlineDelete,
+    AiOutlineEdit,
+    AiOutlineRollback
+} from "react-icons/ai";
+import { MdDone } from "react-icons/md";
 import "./styles.scss";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 TodoList.propTypes = {
     todos: PropTypes.array,
     onDeleteClick: PropTypes.func,
     onEditClick: PropTypes.func,
-    onUpdateStatus: PropTypes.func,
+    onUpdateStatus: PropTypes.func
 };
 
-TodoList.defaultProps = {
-    todos: [],
-    onDeleteClick: null,
-    onEditClick: null,
-    onUpdateStatus: null,
-}
-
-function TodoList(props) {
-    const { todos, onDeleteClick, onEditClick, onUpdateStatus } = props
-
-    //delete one item todo
-    const handleDeleteClick = (todo) => {
-        onDeleteClick(todo)
-    }
-
-    //edit title item todo
-    const handleEditClick = (todo) => {
-        onEditClick(todo)
-    }
-
-    //mark completed item todo
-    const handleUpdateStatus = (todo) => {
-        onUpdateStatus(todo)
-    }
-
+function TodoList({
+    todos = [],
+    onDeleteClick = null,
+    onEditClick = null,
+    onUpdateStatus = null
+}) {
     return (
         <div className="todo-list">
             <ul className="todo-list__items">
@@ -45,17 +30,24 @@ function TodoList(props) {
                         key={todo.id}
                         className={classNames({
                             item: true,
-                            done: todo.status === "done",
+                            done: todo.status === "done"
                         })}
                     >
-                        <p className="content">
-                            {todo.title}
-                        </p>
+                        <p className="content">{todo.title}</p>
                         <div className="icons">
-                            <AiOutlineEdit className="icon" onClick={() => handleEditClick(todo)} />
-                            <AiOutlineDelete className="icon" onClick={() => handleDeleteClick(todo)} />
-                            <MdDone className="icon" onClick={() => handleUpdateStatus(todo)} />
-                            <AiOutlineRollback className="icon" onClick={() => handleUpdateStatus(todo)} />
+                            <AiOutlineEdit
+                                className="icon"
+                                onClick={() => onEditClick(todo)}
+                            />
+                            <AiOutlineDelete
+                                className="icon"
+                                onClick={() => onDeleteClick(todo)}
+                            />
+                            <MdDone className="icon" onClick={() => onUpdateStatus(todo)} />
+                            <AiOutlineRollback
+                                className="icon"
+                                onClick={() => onUpdateStatus(todo)}
+                            />
                         </div>
                     </li>
                 ))}
