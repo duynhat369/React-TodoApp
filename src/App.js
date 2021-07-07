a660a658cb38b97d7efce30bc01de459f39a79ad
Duy Nhat <duynhat719@gmail.com>
2021-07-07 18:20:40 +0700

    Add page preloader

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import React, { useEffect, useState } from "react";
+import PulseLoader from "react-spinners/PulseLoader";
 import "./App.scss";
 import Todo from "./features/Todo";
 
 function App() {
-  return (
-    <div className="app">
-      <Todo />
-    </div>
-  );
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 5000)
+    }, [])
+    return (
+        <React.Fragment>
+            {loading &&
+                <div className="preloader">
+                    <PulseLoader
+                        color={'coral'}
+                        loading={loading}
+                        size={20}
+                    />
+                </div>
+            }
+            {!loading &&
+                <div className="app">
+                    <Todo />
+                </div>
+            }
+        </React.Fragment>
+    );
 }
 
 export default App;
